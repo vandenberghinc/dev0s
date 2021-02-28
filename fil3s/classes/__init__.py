7dde9f2795c29785fa18281b48864fabee812b00
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-02-28 18:15:22 +0100

    Updates
    updates.

--- fil3s/classes/__init__.py
+++ fil3s/classes/__init__.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 
 # defaults.
-from fil3s.classes.defaults import Files,Formats,Code,gfp,gdate
+from fil3s.classes.defaults import Files,Formats,gfp,gdate
 from fil3s.classes.code import Code
