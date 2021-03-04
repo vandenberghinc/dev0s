fb37fccb1969fca073cb3aa73f50729a34ea0a24
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-03-04 12:03:46 +0100

    Updates
    updates.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Note!
+# ' are required, do not use any '.
+
+# setup.
+from setuptools import setup, find_packages
+setup(
+	name='fil3s',
+	version='2.12.7',
+	description='Some description.',
+	url='http://github.com/vandenberghinc/fil3s',
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+	install_requires=[
+            'requests',
+            'pathlib',
+        ])
