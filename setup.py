b5d54a94dd0413c7aaaa101110502b9e87016904
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-03-12 10:41:32 +0100

    Updates
    updates.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 setup(
 	name='dev0s',
-	version='3.7.8',
+	version='3.7.9',
 	description='Some description.',
 	url='http://github.com/vandenberghinc/dev0s',
 	author='Daan van den Bergh',
